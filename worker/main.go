c1f48d198df296fb89d7ad974a147e1200c49c30
Andrew Fitz Gibbon <fitz@temporal.io>
2023-05-08 12:50:37 -0700

    reduce worker logging level

--- worker/main.go
+++ worker/main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	c, err := client.NewLazyClient(client.Options{
 		Logger: zapadapter.NewZapAdapter(
-			zapadapter.NewZapLogger(zapcore.DebugLevel)),
+			zapadapter.NewZapLogger(zapcore.WarnLevel)),
 	})
 	if err != nil {
 		log.Fatalln("Unable to create client", err)
